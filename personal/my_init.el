dbb3103f21a83c2436d8444084cf5a33ab783613
Derek White <derek.white@ul.com>
2018-04-13 18:27:38 -0500

    * Modify settings for parinfer * Change to set vim indent size in clojure mode * Change to disable smartparens when in parinfer mode and re-enable when leaving parinfer mode

--- personal/my_init.el
+++ personal/my_init.el
@@ -1,9 +1,16 @@
-;; Configure parinfer 
+;; Enable / Configure parinfer 
 (require 'use-package)
 (use-package parinfer
   :ensure t
   :bind
-  (("C-," . parinfer-toggle-mode))
+  (:map parinfer-mode-map
+        ("<tab>" . parinfer-smart-tab:dwim-right)
+        ("C-i" . parinfer--reindent-sexp)
+        ("C-M-i" . parinfer-auto-fix)
+        ("C-," . parinfer-toggle-mode)
+        :map parinfer-region-mode-map
+        ("C-i" . indent-for-tab-command)
+        ("<tab>" . parinfer-smart-tab:dwim-right) )
   :init
   (progn
      (setq parinfer-extensions
@@ -11,7 +18,7 @@
          pretty-parens  ; different paren styles for different modes.
          evil           ; If you use Evil.
          ;; lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
-         paredit        ; Introduce some paredit commands.
+         ;;paredit        ; Introduce some paredit commands.
          smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
          smart-yank))   ; Yank behavior depend on mode.
     (add-hook 'clojure-mode-hook #'parinfer-mode)
@@ -19,3 +26,17 @@
     (add-hook 'common-lisp-mode-hook #'parinfer-mode)
     (add-hook 'scheme-mode-hook #'parinfer-mode)
     (add-hook 'lisp-mode-hook #'parinfer-mode)))
+
+(print "Set vim shift action to only use 2 spaces when in clojure mode")
+(defun set-vim-shift-width ()
+    (setq evil-shift-width 2))
+(add-hook 'clojure-mode-hook #'set-vim-shift-width)
+
+(print "Disable smartparens (doesn't work with parinfer)")
+(require 'smartparens-config)
+(defun parinfer-disable-smartparens ()
+    (smartparens-global-mode 0))
+(defun parinfer-enable-smartparens ()
+    (smartparens-global-mode 1))
+(add-hook 'parinfer-mode-enable-hook #'parinfer-disable-smartparens)
+(add-hook 'parinfer-mode-disable-hook #'parinfer-enable-smartparens)
